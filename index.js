0a588a24a0b88955f314b5e73e733d575f60b887
DhanushKumar13 <dhanushpappu99@gmail.com>
2023-04-04 20:51:54 +0530

    added js

---
+++ index.js
@@ -0,0 +1,18 @@
+//Palindrome problem
+// function reverse(str){
+//     var getRid = /[\W_]/g
+
+//     var kuttyName = str.replace(getRid, "")
+
+//     var name = kuttyName.split('').reverse().join('');
+//     console.log(name);
+//     if(name == kuttyName){
+//         console.log("True")
+//         return;
+//     }else{
+//         console.log("False")
+//         return;
+//     }
+// }
+
+// reverse("racecar)*_");
